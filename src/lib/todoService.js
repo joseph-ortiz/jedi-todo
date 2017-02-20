53a39522b6bb85f10eefaf4b722b24f9f1d8cb5b
jortiz <josepho011235@gmail.com>
2017-02-20 05:21:02 -0500

    save todo on server

--- src/lib/todoService.js
+++ src/lib/todoService.js
@@ -2,8 +2,8 @@ const baseUrl = 'http://localhost:8080/todos';
 
 export const loadTodos = () => {
   return fetch(baseUrl)
-    .then(res => res.json())
-}
+    .then(res => res.json());
+};
 
 export const createTodo = (todo) => {
   return fetch(baseUrl, {
@@ -14,4 +14,15 @@ export const createTodo = (todo) => {
     },
     body: JSON.stringify(todo),
   }).then(res => res.json());
-}
+};
+
+export const saveTodo = (todo) => {
+  return fetch(`${baseUrl}/${todo.id}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  }).then(res => res.json());
+};
